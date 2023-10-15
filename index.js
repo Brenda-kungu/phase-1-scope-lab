bb689c35c5145840e41b5b5bf56e2aa34d718528
Brenda <brendakungu3@gmail.com>
2023-10-15 12:51:54 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob";
+function uppercaseCustomerName() {
+    if (typeof customerName === "string") {
+      customerName = customerName.toUpperCase();
+    } else {
+      console.error("customerName is not a string!");
+    }
+  }
+  function declareBestCustomer() {
+    // Declare a variable in the global scope
+    bestCustomer = 'not bob';
+  }
+
+  var bestCustomer = 'not bob';
+
+// Function to overwrite the global variable
+function overwriteBestCustomer(newBestCustomer) {
+  // Modifying the global variable
+  bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'Joe';
+
+// Function attempting to change the constant
+function changeLeastFavoriteCustomer(newLeastFavoriteCustomer) {
+  // This will result in an error
+  leastFavoriteCustomer = newLeastFavoriteCustomer;
+}
+
